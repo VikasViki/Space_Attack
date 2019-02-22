6c16ea76166b2c2031822ebf29890305ae42d547
vikasviki <vikasviki489@gmail.com>
2019-02-22 17:00:40 +0530

    create canvas.js file for game graphics

---
+++ scripts/canvas.js
@@ -0,0 +1,9 @@
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
+
+var canvasElement = $("<canvas widht = '" + CANVAS_WIDTH + 
+                        "' height = '" + CANVAS_HEIGHT + "'></canvas>");
+
+var canvas = canvasElement.get(0).getContext("2d");
+
+canvasElement.appendTo('body');
