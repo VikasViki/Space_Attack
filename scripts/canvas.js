e125a692f05dda165891aee4463eed006ca824cf
vikasviki <vikasviki489@gmail.com>
2019-02-22 17:23:57 +0530

    create canvas file of the game

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,7 +2,7 @@ var CANVAS_WIDTH = 480;
 var CANVAS_HEIGHT = 320;
 
 var canvasElement = $("<canvas widht = '" + CANVAS_WIDTH + 
-                        "' height = '" + CANVAS_HEIGHT + "'></canvas>");
+                        "' height = '" + CANVAS_HEIGHT + "'bgcolor = 'red'></canvas>");
 
 var canvas = canvasElement.get(0).getContext("2d");
 
